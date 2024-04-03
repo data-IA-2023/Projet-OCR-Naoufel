b7fe07ad2570b808486775e51541a574083ae9ec
NaoufelGreta <naoufelboutet2@gmail.com>
2024-04-03 10:56:47 +0200

    dico to bdd

--- Modules.py
+++ Modules.py
@@ -11,19 +11,54 @@ dico_nom_fichier = liste_fichier()
 #for année in dico_nom_fichier['liste_dico_fichier'].keys():
 for titre in dico_nom_fichier['liste_dico_fichier'][2019]:
     DICO_OCR = OCR(titre)
+    # print(DICO_OCR)
     
     DICO_DB = data_tobdd(DICO_OCR["liste"],DICO_OCR["qr_code_data"])
-    print(DICO_OCR["liste"])
-    print(DICO_DB["Produit"])
-    print("total",DICO_DB["Prix_Total"].split(" ")[0])
+    print(DICO_DB)
+    # print(DICO_OCR["liste"])
+    # print(DICO_DB["Produit"])
+    # print("total",DICO_DB["Prix_Total"].split(" ")[0])
     listeprix=[]
+    count = 0
+
+    
     for produit in DICO_DB["Produit"]:
-        print("1",float(produit["qtt"]))
-        print("2",float(produit["prix_unitaire"].split(" ")[0]))
-        produit = float(produit["qtt"])*float(produit["prix_unitaire"].split(" ")[0])
-        print("produit",produit)
+            if produit["qtt"] == "0":
+                count = 1
+                
+                
+            else:
+                produit = float(produit["qtt"])*float(produit["prix_unitaire"].split(" ")[0])
+                listeprix.append(produit)
+    if count == 1:
+         produit['qtt']=round(float(DICO_DB["Prix_Total"].split(" ")[0])-round(sum(listeprix)))/float(element["prix_unitaire"].split(" ")[0])
+         print(" ------------------CHANGEMENT---------------")
+    print("-----------------------------------PRODUIT FINI-------------------------")
+    print(DICO_DB)
+    
+    
+                   
+    
+                   
+                   
+    """for element in DICO_DB["Produit"]:
+        produit = float(element["qtt"])*float(element["prix_unitaire"].split(" ")[0])
         listeprix.append(produit)
-    print("listeprix",sum(listeprix))
+        # print("qtt",element['qtt'])
+        # print("prix",element['prix_unitaire'].split(" ")[0])
+        # print('prod',float(element["qtt"])*float(element["prix_unitaire"].split(" ")[0]))
+        # print("prixtotal",round(float(DICO_DB["Prix_Total"].split(" ")[0])))
+        if element['qtt']== "0":
+            element['qtt']=round(float(DICO_DB["Prix_Total"].split(" ")[0])-round(sum(listeprix)))/float(element["prix_unitaire"].split(" ")[0])
+            print("new",element['qtt'])
+            print("------------------------------houra----------------------------------")"""                   
+            #   print("1",float(produit["qtt"]))
+            #   print("2",float(produit["prix_unitaire"].split(" ")[0]))
+              
+              
+              
+        
+    
 
 
 
--- data_to_db.py
+++ data_to_db.py
@@ -19,23 +19,37 @@ def data_tobdd(liste,qr_code_data):
                     if "Address" in liste[loop]:
                         liste[loop]=liste[loop].replace("Address","")
                         data_todb["Adresse"] = " ".join([liste[loop],liste[loop+1]])
-                    if "Euro" in liste[loop] and "Total" not in liste[loop]:
-                        if "x" in liste[loop]:
-                            if re.search(r'\d+\.\s', liste[loop]):
-                                 corect_phrase = " ".join([liste[loop].split(".")[0],liste[loop].split(".",1)[1].replace('. ', '.')])
-                                 liste[loop] = corect_phrase
-                                 print("ok")
-                            
-                            # if "  "  in liste[loop]:
-                            #      liste_prod.append({"Libélé_produit":liste[loop].split("  ")[0],
-                            #                 "qtt":liste[loop].split("  ")[1][1],
-                            #                 "prix_unitaire":(re.search(r'\d+(\.\s?\d+)?\sEuro',liste[loop].split("  ")[0])).group(0)})
-                                  
-                            liste_prod.append({"Libélé_produit":liste[loop].split(".")[0],
+                    if "TOTAL" in liste[loop]:
+                        #   print("total",liste[loop])
+                          data_todb["Prix_Total"] = " ".join([liste[loop].split(" ")[1],liste[loop].split(" ")[2]])
+                    elif "Euro" in liste[loop]:
+                        # print("euro",liste[loop])
+                        if "  " in liste[loop]:
+                                liste[loop] = liste[loop].replace('  ', '. ')
+                        if re.search(r'\d{2}\. \d{2}', liste[loop]):
+                                # print("ok")
+                                # print(liste[loop])
+                                # print("Libélé_produit",liste[loop].split(".")[0])
+                                # print("qtt",liste[loop].split(".",1)[1][1])
+                                # print("prix_unitaire",(re.search(r'\d+(\.\s?\d+)?\sEuro',liste[loop].split(".",1)[1])).group(0))
+                                # print("phrase avant: ",liste[loop])
+                                liste[loop] =re.sub(r'\. (\d+)', r'.\1', liste[loop])
+                                liste[loop] =re.sub(r'([a-zA-Z])\.(\d+)', r'\1. \2',liste[loop])
+                                # print("phrase après: ",liste[loop])  
+                        if len(liste[loop]) <15:
+                               liste[loop] = f"Inconnu. {liste[loop]}"
+                        if re.search(r'\bX\s+(\d+\.\d+\s+Euro)\b',liste[loop]):
+                              liste[loop] = re.sub(r'\bX\s+(\d+\.\d+\s+Euro)\b', r'0 x \1', liste[loop])
+                            #   print("why not")
+                             
+                              
+                              
+                    
+                        liste_prod.append({"Libélé_produit":liste[loop].split(".")[0],
                                             "qtt":liste[loop].split(".",1)[1][1],
                                             "prix_unitaire":(re.search(r'\d+(\.\s?\d+)?\sEuro',liste[loop].split(".",1)[1])).group(0)})
-                        else:
-                            data_todb["Prix_Total"] = " ".join([liste[loop].split(" ")[1],liste[loop].split(" ")[2]]) 
+                        
+                    
                     data_todb["Produit"]=liste_prod
                     data_todb["Id_Client"]=qr_code_data['CUST']
                     data_todb["Catégorie"]=qr_code_data['CAT']
@@ -75,3 +89,18 @@ def data_tobdd(liste,qr_code_data):
 
 
 #         return data_todb
+
+
+
+ # if re.search(r'\d+\.\s+\d+', liste[loop]):
+                                #  corect_phrase = " ".join([liste[loop].split(".")[0],(liste[loop].split(".",1)[1]).replace('. ', '.')])
+                                #  print("dedans",liste[loop].split(".")[0],"/",liste[loop].split(".",1)[1])
+                                #  liste[loop] = corect_phrase
+                                #  print("listeloppop",liste[loop])
+                                #  print("ok")
+                            #print("dehors")
+                            
+                            # if "  "  in liste[loop]:
+                            #      liste_prod.append({"Libélé_produit":liste[loop].split("  ")[0],
+                            #                 "qtt":liste[loop].split("  ")[1][1],
+                            #                 "prix_unitaire":(re.search(r'\d+(\.\s?\d+)?\sEuro',liste[loop].split("  ")[0])).group(0)})
